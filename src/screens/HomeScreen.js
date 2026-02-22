3409d72595647a987f1e3b281d1bf4543ab91580
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2026-02-22 17:16:23 +0530

    Designed Home Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,44 @@
-import { Text, Button } from 'react-native'
+import { ScrollView, View, useWindowDimensions } from 'react-native'
+import styles from '../styles/homeScreenStyles'
+import MenuItem from '../components/MenuItem'
 
 const HomeScreen = ({ navigation }) => {
+  const iconDirPath = '../../assets/menu/'
+  
+  const menuOptions = [
+    { screen: 'Bank', icon: require(`${iconDirPath}bank.png`), title: 'Bank' },
+    { screen: 'Stock', icon: require(`${iconDirPath}stock.png`), title: 'Stock' }
+  ]
+
+  let screenWidth = useWindowDimensions().width
+  // 20 is the container padding.
+  // 100 + 20 is the item's width + margin.
+  let nItemsInRow = Math.trunc((screenWidth - 20) / (100 + 20))
+  let nItemsInLastRow = menuOptions.length % nItemsInRow
+  if(nItemsInLastRow === 0)
+    nItemsInLastRow = nItemsInRow
+  let nEmptyItemsToAdd = nItemsInRow - nItemsInLastRow
+  
   return (
-    <>
-      <Button title='Bank' onPress={ () => navigation.navigate('Bank') } />
-      <Button title='Stock' onPress={ () => navigation.navigate('Stock') } />
-    </>
+    <ScrollView style={styles.container}>
+      <View style={styles.childContainer}>
+        
+        {menuOptions.map((opt, index) =>
+          <MenuItem
+            navigation={navigation}
+            screen={opt.screen}
+            icon={opt.icon}
+            title={opt.title}
+            key={index}
+          />
+        )}
+
+        {Array.from({length: nEmptyItemsToAdd}).map((_, i) =>
+          <View style={styles.empty} key={i} />
+        )}
+
+      </View>
+    </ScrollView>
   )
 }
 
