e7325a61f1868c3e89226072c279316fd58043a0
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2026-02-22 17:52:20 +0530

    Designed Stock Screen

--- src/screens/StockScreen.js
+++ src/screens/StockScreen.js
@@ -1,8 +1,43 @@
-import { Button } from 'react-native'
+import { ScrollView, View, useWindowDimensions } from 'react-native'
+import styles from '../styles/stockScreenStyles'
+import MenuItem from '../components/MenuItem'
 
 const StockScreen = ({ navigation }) => {
+  const iconDirPath = '../../assets/stock/'
+  
+  const menuOptions = [
+    { screen: 'StockAddTransaction', icon: require(`${iconDirPath}add-transaction.png`), title: 'Transaction' }
+  ]
+
+  let screenWidth = useWindowDimensions().width
+  // 20 is the container padding.
+  // 100 + 20 is the item's width + margin.
+  let nItemsInRow = Math.trunc((screenWidth - 20) / (100 + 20))
+  let nItemsInLastRow = menuOptions.length % nItemsInRow
+  if(nItemsInLastRow === 0)
+    nItemsInLastRow = nItemsInRow
+  let nEmptyItemsToAdd = nItemsInRow - nItemsInLastRow
+  
   return (
-    <Button title='Add Transaction' onPress={ () => navigation.navigate('StockAddTransaction') } />
+    <ScrollView style={styles.container}>
+      <View style={styles.childContainer}>
+        
+        {menuOptions.map((opt, index) =>
+          <MenuItem
+            navigation={navigation}
+            screen={opt.screen}
+            icon={opt.icon}
+            title={opt.title}
+            key={index}
+          />
+        )}
+
+        {Array.from({length: nEmptyItemsToAdd}).map((_, i) =>
+          <View style={styles.empty} key={i} />
+        )}
+
+      </View>
+    </ScrollView>
   )
 }
 
