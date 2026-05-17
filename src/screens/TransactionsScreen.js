cf8cb4d1b3e088c1a7d23e331644700713abf2af
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2026-05-17 22:27:52 +0530

    Fetching Transactions from server

--- src/screens/TransactionsScreen.js
+++ src/screens/TransactionsScreen.js
@@ -1,6 +1,44 @@
+import { useState, useEffect } from 'react'
 import { Text } from 'react-native'
+import { useIsFocused } from '@react-navigation/native'
+import { API_URL } from '../config'
 
-const TransactionsScreen = () => {
+const TransactionsScreen = ({ route, navigation }) => {
+  const { screenTitle, path, updateInfo } = route.params
+  const [transactions, setTransactions] = useState([])
+  const [loading, setLoading] = useState(true)
+  const isFocused = useIsFocused()
+
+  const getTransactions = async () => {
+    try {
+      const url = `${API_URL}${path}`
+      let params = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      }
+      const res = await fetch(url, params)
+      setLoading(false)
+      if(!res)
+        throw new Error('Unable to fetch transactions!')
+      let resData = await res.json()
+      if(!(resData && Array.isArray(resData)))
+        throw new Error('Unable to fetch transactions!')
+      setTransactions(resData)
+    }
+    catch(err) {
+      Alert.alert(screenTitle, err.message, [{ text: 'OK' }])
+    }
+  }
+
+  useEffect(() => {
+    navigation.setOptions({ title: screenTitle })
+  }, [])
+  
+  useEffect(() => {
+    if(isFocused)
+      getTransactions()
+  }, [isFocused])
+
   return (
     <Text>Transactions Screen!</Text>
   )
